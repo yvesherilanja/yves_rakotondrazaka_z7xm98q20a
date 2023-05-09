acbb0eebc8036bb9adbea29ade55d8b6ad18ec14
yvesherilanja <yvesherilanja16@gmail.com>
2023-05-09 19:16:39 +0300

    init project

---
+++ src/test/java/anywr/test_spring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package anywr.test_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
